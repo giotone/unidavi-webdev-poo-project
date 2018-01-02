72785b134b44304c1eae646d170fa403c24b9210
Giovani Tonet <giotonet@gmail.com>
2018-01-02 17:52:24 -0200

    commit com configuração para empacotar código fonte e javadoc como jar

--- src/test/java/TrabalhoFinalTest.java
+++ src/test/java/TrabalhoFinalTest.java
@@ -4,8 +4,14 @@
  * and open the template in the editor.
  */
 
+import TrabalhoFinal_Entrega_2.EquipeBasquete;
+import TrabalhoFinal_Entrega_2.IniciaListaEquipe;
+import TrabalhoFinal_Entrega_2.Jogador;
+import TrabalhoFinal_Entrega_2.JogoBasquete;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -42,5 +48,35 @@ public class TrabalhoFinalTest {
      @Test
      public void hello() {
      
+     }
+     @Test
+     public void testIniciaListaEquipe(){
+         
+        IniciaListaEquipe listaEquipe = new IniciaListaEquipe();
+        List<EquipeBasquete> equipes;
+
+        equipes = listaEquipe.getEquipe();         
+        Assert.assertEquals(4, equipes.size());
+     }
+     
+     @Test
+     public void testAdicionaEquipe(){
+         
+        IniciaListaEquipe listaEquipe = new IniciaListaEquipe();
+        List<EquipeBasquete> equipes;
+         
+        equipes = listaEquipe.getEquipe();
+
+        EquipeBasquete bulls = new EquipeBasquete("Chicago Bulls","2017-2018");
+        bulls.setVitoria(0);
+        bulls.setDerrota(0);
+        bulls.addJogador(new Jogador("Lauri Markkanen",24));
+        bulls.addJogador(new Jogador("Denzel Valentine",45));
+        bulls.addJogador(new Jogador("Robin Lopez",42));
+        bulls.addJogador(new Jogador("Justin Holiday",7));
+        
+        equipes.add(bulls);
+         
+         Assert.assertEquals(5, equipes.size());
      }
 }
